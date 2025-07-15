d312e3f59ec29338f6107f01e1f51537fec7e725
DESKTOP-315IGCU\TravelierIT <oxygenonline@gmail.com>
2025-07-15 17:54:44 +0300

    setup scripts

---
+++ setup/createTopics.bat
@@ -0,0 +1,4 @@
+docker-compose exec broker /opt/kafka/bin/kafka-topics.sh -create --topic soccer-scores --bootstrap-server broker:29092
+docker-compose exec broker /opt/kafka/bin/kafka-topics.sh -create --topic football-scores --bootstrap-server broker:29092
+docker-compose exec broker /opt/kafka/bin/kafka-topics.sh -create --topic basketball-scores --bootstrap-server broker:29092
+docker-compose exec broker /opt/kafka/bin/kafka-topics.sh -create --topic baseball-scores --bootstrap-server broker:29092
